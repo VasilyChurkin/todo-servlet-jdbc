ac11a50b121d504d4aefa20342d431165b3752dc
Denis Bogdanov <bogdanov.math@gmail.com>
2018-09-22 00:13:42 +0300

    Add homework for Vasya.

---
+++ src/main/java/ru/bogdanov/todo/dao/JdbcTodoDaoImpl.java
@@ -0,0 +1,42 @@
+package ru.bogdanov.todo.dao;
+
+import ru.bogdanov.todo.model.Todo;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Denis, 22.09.2018
+ */
+public class JdbcTodoDaoImpl implements TodoDao {
+
+    @Override
+    public void add(Todo todo) {
+
+    }
+
+    @Override
+    public Todo findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Todo> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Todo todo) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public List<Todo> search(String searchString) {
+        return null;
+    }
+}
